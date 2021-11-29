7aeaaeabe0772354518a66706b4a28e861d13894
Omar Saucedo <omar.saucedo@wizeline.com>
2021-11-29 01:05:27 -0600

    refactor: remove unnecesary print

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/omarscd/academy-go-q42021/datastore"
 	"github.com/omarscd/academy-go-q42021/registry"
 	"github.com/omarscd/academy-go-q42021/router"
@@ -14,7 +12,5 @@ func main() {
 	r := registry.NewRegistry(&pkMap)
 
 	router := router.NewRouter(r.NewAppController())
-
-	fmt.Println("Listening on port 8888")
 	router.Run("localhost:8888")
 }

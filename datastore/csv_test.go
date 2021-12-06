dcd90879c2b6072c16a7d91d96bd07640a47bd8d
Omar Saucedo <omar.saucedo@wizeline.com>
2021-12-06 02:53:49 -0600

    add: datastore test

---
+++ datastore/csv_test.go
@@ -0,0 +1,92 @@
+package datastore
+
+import (
+	"testing"
+
+	"github.com/omarscd/academy-go-q42021/model"
+)
+
+var mockPokemonDB = PokemonDB{
+	pkMap: map[uint64]model.Pokemon{
+		1: {
+			ID:       1,
+			Name:     "bulbasaur",
+			MainType: "grass",
+		},
+		4: {
+			ID:       4,
+			Name:     "charmander",
+			MainType: "fire",
+		},
+		9: {
+			ID:       9,
+			Name:     "blastoise",
+			MainType: "water",
+		},
+	},
+	path: "./test.csv",
+}
+
+func TestDatastore_Find(t *testing.T) {
+	tests := []struct {
+		testFunc       func(pk model.Pokemon) bool
+		expectedLength int
+	}{
+		{
+			func(pk model.Pokemon) bool { return true },
+			3,
+		},
+		{
+			func(pk model.Pokemon) bool { return pk.ID == 1 },
+			1,
+		},
+		{
+			func(pk model.Pokemon) bool { return pk.Name == "Romario" },
+			0,
+		},
+	}
+
+	for _, tt := range tests {
+		gotPks, gotErr := mockPokemonDB.Find(tt.testFunc)
+		if gotErr != nil {
+			t.Error("Got unexpected error: ", gotErr)
+		}
+		if len(gotPks) != tt.expectedLength {
+			t.Errorf("Should get len 3, instead got %v\n", len(gotPks))
+		}
+	}
+}
+
+func TestDatastore_FindOne(t *testing.T) {
+	tests := []struct {
+		testFunc     func(pk model.Pokemon) bool
+		expectedPk   *model.Pokemon
+		expectsError bool
+	}{
+		{
+			func(pk model.Pokemon) bool { return pk.ID == 1 },
+			&model.Pokemon{
+				ID:       1,
+				Name:     "bulbasaur",
+				MainType: "grass",
+			},
+			false,
+		},
+		{
+			func(pk model.Pokemon) bool { return pk.Name == "Romario" },
+			nil,
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		gotPk, gotErr := mockPokemonDB.FindOne(tt.testFunc)
+		if gotErr != nil {
+			if !tt.expectsError {
+				t.Error("Got unexpected error: ", gotErr)
+			}
+		} else if gotPk.ID != tt.expectedPk.ID {
+			t.Errorf("expected %v, instead got %v\n", tt.expectedPk, gotPk)
+		}
+	}
+}
